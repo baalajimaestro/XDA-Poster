6f74322414659596ab9602f2c926a7a75df6e9aa
baalajimaestro <baalajimaestro@pixelexperience.org>
2020-02-04 20:17:19 +0530

    poster: Use sentry-logger for logging all errors
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

--- get_api_key.py
+++ get_api_key.py
@@ -13,66 +13,79 @@ import re
 from os import remove
 from os import environ
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk import capture_exception
 
-load_dotenv("config.env")
-
+sentry_sdk.init("https://8a6cee16d17a43ec916bafc296b61110@sentry.io/2240523")
 # Run Chrome Headless
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
+try:
+    load_dotenv("config.env")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920x1080")
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.implicitly_wait(30)
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(30)
 
-# Grab the current window
-main_window_handle = None
-while not main_window_handle:
-    main_window_handle = driver.current_window_handle
+    # Grab the current window
+    main_window_handle = None
+    while not main_window_handle:
+        main_window_handle = driver.current_window_handle
+
+    driver.get("http://api.xda-developers.com/explorer/")
+    que=driver.find_element_by_id('login_btn')
+    que.click()
+
+    # Swap to signin window
+    signin_window_handle = None
+    while not signin_window_handle:
+        for handle in driver.window_handles:
+            if handle != main_window_handle:
+                signin_window_handle = handle
+                break
+    driver.switch_to.window(signin_window_handle)
 
 
-driver.get("http://api.xda-developers.com/explorer/")
-que=driver.find_element_by_id('login_btn')
-que.click()
+    username = driver.find_element_by_name("username")
+    password = driver.find_element_by_name("password")
+    submit = driver.find_element_by_id("signin-submit")
+    username.send_keys(environ.get("XDA_USERNAME"))
+    password.send_keys(environ.get("XDA_PASSWORD"))
+    submit.click()
+    driver.implicitly_wait(3)
+    confirm_btn = driver.find_element_by_id("authorize")
+    confirm_btn.click()
 
-# Swap to signin window
-signin_window_handle = None
-while not signin_window_handle:
-    for handle in driver.window_handles:
-        if handle != main_window_handle:
-            signin_window_handle = handle
+    # Come back to main window
+    driver.switch_to.window(main_window_handle)
+    driver.implicitly_wait(3)
+
+    #Scrape out the API Key
+    page = driver.page_source
+    f = open("page_sauce.txt", "w")
+    f.write(page)
+    f.close()
+
+    for line in open("page_sauce.txt", 'r'):
+        if re.search("Access token:", line):
+            token_line = line
             break
-driver.switch_to.window(signin_window_handle)
 
+    remove("page_sauce.txt")
+    token = token_line.split(" ")[5]
+    token = token[:40]
+    print("API-TOKEN: ",token)
 
-username = driver.find_element_by_name("username")
-password = driver.find_element_by_name("password")
-submit = driver.find_element_by_id("signin-submit")
-username.send_keys(environ.get("XDA_USERNAME"))
-password.send_keys(environ.get("XDA_PASSWORD"))
-submit.click()
-driver.implicitly_wait(3)
-confirm_btn = driver.find_element_by_id("authorize")
-confirm_btn.click()
+    environ["XDA_API_TOKEN"] = str(token)
 
-# Come back to main window
-driver.switch_to.window(main_window_handle)
-driver.implicitly_wait(3)
+    # close the browser window
+    driver.quit()
 
-#Scrape out the API Key
-page = driver.page_source
-f = open("page_sauce.txt", "w")
-f.write(page)
-f.close()
-
-for line in open("page_sauce.txt", 'r'):
-    if re.search("Access token:", line):
-        token_line = line
-        break
-
-remove("page_sauce.txt")
-token = token_line.split(" ")[5]
-token = token[:40]
-environ["XDA_API_TOKEN"] = str(token)
-
-# close the browser window
-driver.quit()
+except BaseException as e:
+    driver.quit()
+    x = capture_exception(e)
+    msg="Extraction of API Key Failed!\n" \
+        "The developer was notified.\n" \
+        "Error: "+str(type(e).__name__) + "\n" \
+        "Error ID: "  + x
+    print(msg)
--- poster.py
+++ poster.py
@@ -7,17 +7,30 @@
 
 import json, requests, os
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk import capture_exception
 
-load_dotenv("config.env")
+sentry_sdk.init("https://8a6cee16d17a43ec916bafc296b61110@sentry.io/2240523")
 
-filename=os.environ.get("FILE",None)
-XDA_API_KEY=os.environ.get("XDA_API_KEY",None)
-XDA_THREAD_ID=os.environ.get("XDA_THREAD_ID",None)
-r = requests.get("https://api.xda-developers.com/v3/posts",params={"threadid":XDA_THREAD_ID})
-XDA_POST_ID=r.json()["results"][0]["postid"]
-with open(filename) as file:
-  cl=file.read()
-  cl+="\n\nThis Post was sent from a XDA-Poster Script by @baalajimaestro"
-  data={"postid":XDA_POST_ID,"message":cl}
-  headers = {'Content-type': 'application/json', 'Authorization': 'Bearer '+XDA_API_KEY}
-r = requests.post('https://api.xda-developers.com/v3/posts/new', data=json.dumps(data), headers=headers)
+try:
+    load_dotenv("config.env")
+
+    filename=os.environ.get("FILE",None)
+    XDA_API_KEY=os.environ.get("XDA_API_KEY",None)
+    XDA_THREAD_ID=os.environ.get("XDA_THREAD_ID",None)
+    r = requests.get("https://api.xda-developers.com/v3/posts",params={"threadid":XDA_THREAD_ID})
+    XDA_POST_ID=r.json()["results"][0]["postid"]
+    with open(filename) as file:
+        cl=file.read()
+        cl+="\n\nThis Post was sent from a XDA-Poster Script by @baalajimaestro"
+        data={"postid":XDA_POST_ID,"message":cl}
+        headers = {'Content-type': 'application/json', 'Authorization': 'Bearer '+XDA_API_KEY}
+    r = requests.post('https://api.xda-developers.com/v3/posts/new', data=json.dumps(data), headers=headers)
+
+except BaseException as e:
+    x = capture_exception(e)
+    msg="Posting to XDA Failed!\n" \
+        "The developer was notified.\n" \
+        "Error: "+str(type(e).__name__) + "\n" \
+        "Error ID: "  + x
+    print(msg)
